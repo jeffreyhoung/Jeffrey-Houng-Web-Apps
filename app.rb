b67b10598aba02480f91628edea2e51f0779aa38
jhoung <houng.jeffrey@gmail.com>
2016-11-19 18:23:03 -0500

    picture trials

--- app.rb
+++ app.rb
@@ -59,13 +59,22 @@ get '/incoming_sms' do
     message = get_commands
   
   elsif body == "about" or body == "1"
-    message = "🤓 Jeff is a UX & Product Designer, maker, & tinkerer-fueled on curiosity and cortados. Jeff believes in exposing creativity, magic, and empowerment through the intersection of physical and digital experiences. \n \n He\'s currently finishing up his senior year, studying Product Design at Carnegie Mellon University! \n \n As a naturally curious individual, Jeff has explored many different interests that inform his thinking - resulting in envisioning future retail experiences with Microsoft and American Eagle, creating powerful data visualization experiences for SYMKALA, and designing prototypes for connected childrens' toys and sunglasses for local Pittsburgh startups. \n \n Jeff is originally from New Jersey, but now calls Pittsburgh home with his wife -  Lydia, and son - Nathan! \n \n Jeff is almost finished up with school, so a full-time job is on his radar.  \n \n Check out his portfolio! http://jeffreyhoung.com" + get_menu
+    message = "🤓 Jeff is a UX & Product Designer, maker, & tinkerer-fueled on curiosity and cortados ☕️. Jeff believes in exposing creativity, magic, and empowerment through the intersection of physical and digital experiences. \n \n He\'s currently finishing up his senior year, studying Product Design at Carnegie Mellon University! \n \n As a naturally curious individual, Jeff has explored many different interests that inform his thinking - resulting in envisioning future retail experiences with Microsoft and American Eagle, creating powerful data visualization experiences for SYMKALA, and designing prototypes for connected childrens' toys and sunglasses for local Pittsburgh startups. \n \n Jeff is originally from New Jersey, but now calls Pittsburgh home with his wife -  Lydia, and son - Nathan! \n \n Jeff is almost finished up with school, so a full-time job is on his radar.  \n \n Check out his portfolio! http://jeffreyhoung.com" + get_menu
     
   elsif body == "resume" or body == "2"
     message = "1) Microsoft - UX Design Intern \n \n 2) SYMKALA - Product Designer \n \n 3) BikePGH/PositiveSpin - Cycling Assistant Intern \n \n 4) Transatlantic Climate Bridge - Visual Designer \n \n 5) Look Eyewear - Industrial Design Contractor \n \n 6) AE Dreams - Prototyping Contractor" + " \n \n \n choose number to learn more about each" + get_menu
  
-  elsif body == "work" or body == "3"
-    message = "View Jeff's work on his portfolio at http://jeffreyhoung.com" + get_menu
+ elsif body == "work" or body == "3"
+   pic = "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAaDAAAAJDY4ZDI5NWMzLTM1MGQtNDc0NC05Mzk2LWIwZDYxYzQ3ZmYzNA.jpg"
+   message = "View Jeff's work on his portfolio at http://jeffreyhoung.com"  + get_menu
+  
+   twiml = Twilio::TwiML::Response.new.do |r|
+    r. Message do |m|
+      m.Body message
+      m.Media pic
+      
+    
+    
     
   elsif body == "play" or body == "4"
     message = "I don't do much but I do it well. You can ask me who what when where or why." + get_menu
