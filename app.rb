21f1cbdcca33806258e911760c6ca9f0c314d3c0
jhoung <houng.jeffrey@gmail.com>
2016-11-19 16:28:08 -0500

    clean up

--- app.rb
+++ app.rb
@@ -90,44 +90,44 @@ get '/incoming_sms' do
   
   if body == "hi" or body == "hello" or body == "hey" or body == "yo" or body == "sup" or body == "What's up" or body == "heyo" or body == "hey!"or body == "hi!"
     message = get_about_message + "\n" + "Want to know a little bit more about Jeff? \n" + get_commands
-  end
     
-  if body == "back" or body == "menu" or body == "home"
+  elsif body == "back" or body == "menu" or body == "home"
     message = get_commands
-  end
+
   
-  if body == "about" or body == "1"
+  elsif body == "about" or body == "1"
     message = "🤓 Jeff is a UX & Product Designer, maker, & tinkerer-fueled on curiosity and cortados. Jeff believes in exposing creativity, magic, and empowerment through the intersection of physical and digital experiences. \n \n He\'s currently finishing up his senior year, studying Product Design at Carnegie Mellon University! \n \n As a naturally curious individual, Jeff has explored many different interests that inform his thinking - resulting in envisioning future retail experiences with Microsoft and American Eagle, creating powerful data visualization experiences for SYMKALA, and designing prototypes for connected childrens' toys and sunglasses for local Pittsburgh startups. \n \n Jeff is originally from New Jersey, but now calls Pittsburgh home with his wife -  Lydia, and son - Nathan! \n \n Jeff is almost finished up with school, so a full-time job is on his radar.  \n \n Check out his portfolio! http://jeffreyhoung.com" + get_menu
-  end
+  
     
-  if body == "resume" or body == "2"
+  elsif body == "resume" or body == "2"
    message = "1) Microsoft - UX Design Intern \n \n 2) SYMKALA - Product Designer \n \n 3) BikePGH/PositiveSpin - Cycling Assistant Intern \n \n 4) Transatlantic Climate Bridge - Visual Designer \n \n 5) Look Eyewear - Industrial Design Contractor \n \n 6) AE Dreams - Prototyping Contractor" + " \n \n \n choose number to learn more about each" + get_menu
  
     
-  if body == "work" or body == "3"
+  elsif body == "work" or body == "3"
     message = "I was made by Daragh." + get_menu
-  end
+
   
   
-  if body == "play" or body == "4"
+  elsif body == "play" or body == "4"
     message = "I don't do much but I do it well. You can ask me who what when where or why." + get_menu
-  end 
+ 
     
-  if body == "beats" or body == "5"
+  elsif body == "beats" or body == "5"
     message = Time.now.strftime( "It's %A %B %e, %Y") + get_menu
-  end
+
     
-  if body == "Let's talk" or body == "6"  or body == "📞" or body == "lets talk" or body == "let's talk"  
+  elsif body == "Let's talk" or body == "6"  or body == "📞" or body == "lets talk" or body == "let's talk"  
     message = get_menu
+
+  
+  else
+    message = error_response
+    session["last_context"] = "error"
   end
-  
-  # else
-#     message = error_response
-#     session["last_context"] = "error"
-#   end
-  
-  # COMMANDS = "about, work, play, beats, and work experience."
-  
+
+
+
+
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message message
   end
@@ -197,4 +197,5 @@ def error_response
   error_prompt = ["I didn't catch that.", "Hmmm I don't know that word.", "What did you say to me? "].sample
   error_prompt + " " + get_commands
 end
+
 
