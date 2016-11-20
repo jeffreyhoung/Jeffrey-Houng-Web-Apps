88ba26a7a8fabf6db39e7b42f7e7f26a7868b93d
jhoung <houng.jeffrey@gmail.com>
2016-11-19 23:08:15 -0500

    sample again

--- app.rb
+++ app.rb
@@ -75,7 +75,7 @@ get '/incoming_sms' do
   #
     
     elsif body == "random facts" or body == "4" or body == "random"
-        message = "Random Fact:" + ["Jeff loves sushi soooo much!", "Jeff is part Japanese 🇯🇵, part Taiwanese 🇹🇼, and part Dutch, 🇳🇱", "Jeff was born in Eatontown, NJ", "Jeff can't take eat too many sweets...unless they are pastries! 🍩"].sample + get_menu
+        message = "Random Fact:" + ["Jeff loves sushi soooo much!", "Jeff is part Japanese 🇯🇵, part Taiwanese 🇹🇼, and part Dutch, 🇳🇱", "Jeff was born in Eatontown, NJ", "Jeff can't take eat too many sweets...unless they are pastries! 🍩"].sample + "type 'random' again to see another fact!" + get_menu
     
     elsif body == "beats" or body == "5"
       message = Time.now.strftime( "It's %A %B %e, %Y") + "currently listening to Chill Tracks Playlist: \n https://open.spotify.com/user/spotify/playlist/6VXeTHZPxzx3SGJvHJj80n" + get_menu
@@ -129,7 +129,7 @@ private
 
 GREETINGS = ["Hey","Yo", "Sup","Hi", "Hello", "Ahoy", "‘Ello", "Aloha", "Hola", "Bonjour", "Hallo", "Ciao", "Konnichiwa"]
 
-COMMANDS = "1) about" + "\n" + "2) resume" + "\n" + "3) work" + "\n" + "4) play" + "\n" + "5) beats" + "\n" +  "6) Let's talk! 📞 "
+COMMANDS = "1) about" + "\n" + "2) resume" + "\n" + "3) work" + "\n" + "4) random facts" + "\n" + "5) beats" + "\n" +  "6) Let's talk! 📞 "
 
 def get_menu
   "\n \n Type 'menu' or 'back' at anytime to go back."
